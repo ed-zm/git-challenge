1d087654851f18322d685a5c5af504325e6033d1
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2019-10-24 15:43:01 -0400

    fix unacceptable errors

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ function respondHello (req, res) {
 function respondUserAgent (req, res) {
   const ua = req.headers['user-agent']
   res.end(JSON.stringify({ ua }))
+}
+
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
